32d3ed9a7b9710e88233e2afa95b2502104cd0e2
Manish Kumar <66414715+kumarmannish@users.noreply.github.com>
2023-07-31 13:04:50 +0530

    Add files via upload

---
+++ vagrant/8. automate_project/memcache.sh
@@ -0,0 +1,14 @@
+sudo yum install epel-release -y
+sudo yum install memcached -y
+sudo systemctl start memcached
+sudo systemctl enable memcached
+sudo systemctl status memcached
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/sysconfig/memcached
+sudo systemctl restart memcached
+sudo systemctl start firewalld
+sudo systemctl enable firewalld
+sudo firewall-cmd --add-port=11211/tcp
+sudo firewall-cmd --runtime-to-permanent
+sudo firewall-cmd --add-port=11111/udp
+sudo firewall-cmd --runtime-to-permanent
+sudo memcached -p 11211 -U 11111 -u memcached -d
